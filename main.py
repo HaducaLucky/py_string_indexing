6e4275e7eace3b23bf32d467ac060096f9a82fb7
LUCKY FERNANDEZ HADUCA <147017329+HaducaLucky@users.noreply.github.com>
2024-10-26 21:20:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#! indexing = accessing elements of a sequence using [] (indexing operator)
+#*            [start : end : step]
+
+# credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[1])
+# print(credit_number[0:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-7])
+# print(credit_number[::3])
+
+#TODO: practical example
+
+# credit_number = "1234-5678-9012-3456"
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+#TODO: reverse the string
+
+credit_number = "1234-5678-9012-3456"
+
+credit_number = credit_number[::-1]
+print(credit_number)
